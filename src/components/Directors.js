b21b647e7c9ee5c37b077fb4ee45c70a3b7f8bff
Ely Karr <elykarr@gmail.com>
2021-12-27 09:25:22 -0800

    in progress, render error

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,17 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h2>Directors Page</h2>
+    {directors.map(director => {
+      return <div>{director}
+        <ul>Movies</ul>
+        {director.movies.map(movie => {
+          return <li>{movie}</li>
+        })}
+      </div>
+    })}
+  </div>;
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,20 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Movies Page</h1>
+    {movies.map(movie => {
+      return <div>
+        <h2>{movie.title}</h2>
+        <h3>Runtime:{movie.time}</h3>
+        <ul>Genres</ul>
+        {movie.genres.map(genre => {
+          return <li>{genre}</li>
+        })
+        }
+      </div>
+    })}
+  </div>;
 }
 
 export default Movies;
